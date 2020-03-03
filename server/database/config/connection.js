f472e67a7cb283247a1a8e215943aff7afc6a442
MohammedIrheem <mohammedirheem96@gmail.com>
2020-03-03 13:03:39 +0200

    create connection file relates #7

---
+++ server/database/config/connection.js
@@ -0,0 +1,22 @@
+require('env2').config('./config.env');
+
+const { Pool } = require('pg')
+
+const dbUrl = '';
+
+if (process.env.NODE_ENV === 'test') {
+  dbUrl = process.env.DB_URL_TEST
+} else if (process.env.NODE_ENV === 'development') {
+  dbUrl = process.env.DB_URL_DEV;
+} else {
+  dbUrl = process.env.DB_URL;
+}
+
+if (!dbUrl) throw new Error('THERE IS NO DATABASE!')
+
+const option = {
+  connectionString: dbUrl,
+  ssl: true
+}
+
+module.exports = new Pool(option);
