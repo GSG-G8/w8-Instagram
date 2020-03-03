141166a0db9ff890e71006253310afc73d3b1718
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-03-03 14:24:18 +0200

    hashPassword and comparePasswords relates #15

--- server/controller/hash.js
+++ server/controller/hash.js
@@ -1,6 +1,27 @@
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcryptjs');
+
+const hashPassword = (password) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) reject(err);
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) reject(err);
+        else resolve(hash);
+      });
+    });
+  });
+}
+
+const comparePasswords = (password, hash) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, hash, (err, valid) => {
+      if (err) reject(err);
+      else resolve(valid);
+    })
+  })
+}
 
 module.exports = {
-  hashPassword: bcrypt.hashSync,
-  comparePasswords: bcrypt.compareSync
+  hashPassword,
+  comparePasswords
 };
