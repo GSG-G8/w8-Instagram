ece1bbd70faccf22d52171fa509f551e6c09b602
rehabas <reremo45@gmail.com>
2020-03-03 14:24:48 +0200

    add user relates #22

---
+++ server/database/query/users/addUser.js
@@ -0,0 +1,8 @@
+const dbConnection = require('../../config/connection');
+
+const insertUser = (reqData) => dbConnection.query({
+  text: 'INSERT INTO users(name, email, password) VALUES ($1, $2, $3)',
+  values: [reqData.name, reqData.email, reqData.password],
+});
+
+module.exports = { insertUser };
