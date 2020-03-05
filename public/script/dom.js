2c64590e927f49fd11f1ec41d52143b7bb2ee61e
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-03-05 09:12:31 +0200

    render posts

--- public/script/dom.js
+++ public/script/dom.js
@@ -1,7 +1,9 @@
 
 const modal = document.getElementById('myModal');
 const newPost = document.querySelector('.new-post');
-const span = document.getElementsByClassName('close')[0];
+const span = document.querySelector('.close');
+
+const postContainer = document.querySelector('.posts');
 
 newPost.addEventListener('click', () => {
   modal.style.display = 'block';
@@ -12,51 +14,46 @@ span.addEventListener('click', () => {
 
 function createPostElement(obj) {
   const postsItem = document.createElement('div');
-
   const postsUser = document.createElement('div');
-  const postsFolio = document.createElement('div');
-  const postsPhoto = document.createElement('div');
-  const postsUsername = document.createElement('h1');
-
   const postsPost = document.createElement('div');
+  const postsFolio = document.createElement('div');
+  const postsUsername = document.createElement('h1');
+  const postsPhoto = document.createElement('img');
   const postsThumbnail = document.createElement('div');
-  const postsImage = document.createElement('img');
-
   const postsDetails = document.createElement('div');
+  const postsImage = document.createElement('img');
   const postsTitle = document.createElement('h2');
   const postsContent = document.createElement('p');
 
-  postsFolio.appendChild(postsPhoto);
-  postsUser.appendChild(postsFolio);
-  postsUser.appendChild(postsUsername);
-
-
-  postsThumbnail.appendChild(postsImage);
-
-  postsDetails.appendChild(postsTitle);
-  postsDetails.appendChild(postsContent);
-
-  postsPost.appendChild(postsThumbnail);
-  postsPost.appendChild(postsDetails);
+  postsItem.className = 'posts__item';
+  postsUser.className = 'posts__user';
+  postsPost.className = 'posts__post';
+  postsFolio.className = 'posts__folio';
+  postsPhoto.className = 'posts__img';
+  postsThumbnail.className = 'posts_thumbnail';
+  postsPhoto.src = "/images/user.png";
+  postsImage.src = obj.image;
+  postsTitle.textContent = obj.title;
+  postsContent.textContent = obj.details;
+  postsUsername.textContent = 'Matt Johney';
 
   postsItem.appendChild(postsUser);
   postsItem.appendChild(postsPost);
+  postsUser.appendChild(postsFolio);
+  postsUser.appendChild(postsUsername);
+  postsPost.appendChild(postsThumbnail);
+  postsPost.appendChild(postsDetails);
+  postsFolio.appendChild(postsPhoto);
+  postsThumbnail.appendChild(postsImage);
+  postsDetails.appendChild(postsTitle);
+  postsDetails.appendChild(postsContent);
 
-  postsItem.className = 'posts__item';
-
-  postsUser.className = 'posts__user';
-  postsFolio.className = 'posts__folio';
-  postsPhoto.className = 'posts__img';
-
-  postsPost.className = 'posts__post';
-  postsThumbnail.className = 'posts_thumbnail';
-  postsImage.src = obj.image;
-
-  postsDetails.className = 'posts__user';
-  postsTitle.textContent = obj.title;
-  postsContent.textContent = obj.details;
   return postsItem;
 }
 
 function renderPosts(arr) {
+  postContainer.textContent = '';
+  arr.forEach((post) => {
+    postContainer.appendChild(createPostElement(post));
+  });
 }
